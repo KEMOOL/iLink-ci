4c0ba37c57b10e2123157eeeb5de91fbaa08c7e6
KEMOOL <ahmadmufid06@gmail.com>
2019-10-30 21:15:18 +0700

    penggabungan css

--- assets/global.js
+++ assets/global.js
@@ -5,7 +5,3 @@ function globalInclude() {
     document.write('<script src="' + url + '/assets/js/jquery-3.4.1.js"></script>');
     document.write('<script src="' + url + '/assets/js/bootstrap.js"></script>');
 }
-/* <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.css"></link>
-<link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css"></link>
-<script src="<?= base_url(); ?>assets/js/jquery-3.4.1.js"></script>
-<script src="<?= base_url(); ?>assets/js/bootstrap.js"></script> */
