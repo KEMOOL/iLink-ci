7819f5817282580bd8c847b7dc6bc724921c8ed1
KEMOOL <ahmadmufid06@gmail.com>
2019-11-12 16:21:14 +0700

    njajal ahh

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -75,12 +75,24 @@ class Auth extends CI_Controller
         if ($this->form_validation->run() == false) {
             $this->load->view('auth/registration');
         } else {
+            $email = $this->input->post('email');
             $data = [
                 'username' => htmlspecialchars($this->input->post('username')),
-                'email' => htmlspecialchars($this->input->post('email')),
+                'email' => htmlspecialchars($email),
                 'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT)
             ];
+
+            $token = base64_encode(random_bytes(64));
+            $token_daftar = [
+                'email' => $email,
+                'token' => $token
+            ];
+
             $this->db->insert('user', $data);
+            // $this->db->insert('pendaftaran', $token_daftar);
+
+            $this->_sendEmail($token);
+
             $this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">Selamat</div>');
             redirect('auth');
         }
@@ -93,6 +105,7 @@ class Auth extends CI_Controller
         $password = $this->input->post('password');
         $repeat_password = $this->input->post('repeat-password');
         $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        $usernamedb = $this->db->get_where('user', ['username' => $username])->row_array();
 
         if ($email == '' || $password == '' || $username == '' || $repeat_password == '') {
             $this->form_validation->set_message('registration_check', 'Form Kosong');
@@ -103,7 +116,35 @@ class Auth extends CI_Controller
         } elseif ($user) {
             $this->form_validation->set_message('registration_check', 'Email Sudah Pernah Terdaftar');
             return FALSE;
+        } elseif ($usernamedb) {
+            $this->form_validation->set_message('registration_check', 'Username Sudah Pernah Terdaftar');
+            return FALSE;
         }
+    }
+
+    private function _sendEmail($token)
+    {
+        $config = [
+            'protocol'  => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_user' => 'xyz.ilink@gmail.com',
+            'smtp_pass' => 'xyzsemangat',
+            'smtp_port' => 465,
+            'mailtype' => 'html',
+            'charset' => 'utf-8',
+            'newline' => "\r\n"
+        ];
+
+        $this->email->initialize($config);
+
+        $this->email->from('xyz.ilink@gmail.com', 'ILINK');
+        $this->email->to($this->input->post('email'));
+        $this->email->subject('Account Verification');
+        $this->email->message('Hello sayang, nanti malem jadi kan? kalo jadi hubungin aku disini : <a href="' . base_url() . 'auth/verifikasi?email=' . $this->input->post('email') . '&token=' . $token . '">MUAHH</a>');
+
+        if ($this->email->send()) {
+            return true;
+        } else { }
     }
 
     function logout()
